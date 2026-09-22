f031a5bb750b8e5ef76405e02d6c64758856f714
tallalmohar <tallalmoohar@gmail.com>
2026-09-22 00:55:03 -0700

    added operator = assignment constructor

--- custom_std::vector/Vector.h
+++ custom_std::vector/Vector.h
@@ -1,28 +1,32 @@
+#include <iostream>
 
 template <class T>
-class Vector{
+class Vector
+{
 public:
-    Vector(std::size_t capacity);
-    ~Vector();
-    std::size_t size() const;
-    bool empty() const;
-    std::size_t get_capacity() const;
-    T front() const;
-    T back() const;
-    void reserve(std::size_t n);
-    void push_back(T value);
-    void pop_back();
-    void clear();
-    void shrink_to_fit();
-    void resize(std::size_t n);
+  Vector(std::size_t capacity);                 // Constructor
+  Vector(const Vector<T> &other);               // Copy Constructor
+  Vector<T> &operator=(const Vector<T> &other); // does a = b;
+  ~Vector();                                    // Destructor
+  std::size_t size() const;                     // returns a number of elements
+  bool empty() const;                           // checks if std::vector is empty
+  std::size_t get_capacity() const;             // returns capacity
+  T front() const;                              // returns element at index 0
+  T back() const;                               // returns last element
+  void reserve(std::size_t n);                  // reserves storage
+  void push_back(T value);                      // adds element to array, reallocates memory if necessary
+  void pop_back();                              // removes last element from array
+  void clear();                                 // clears the array and frees memory
+  void shrink_to_fit();                         // shrinks array capacity to size
+  void resize(std::size_t n);                   // resizes array + memory to size n
 
 private:
-    std::size_t capacity_ {};
-    T* data_ = nullptr;
-    std::size_t size_ {};
+  std::size_t capacity_{};
+  T *data_ = nullptr;
+  std::size_t size_{};
 
-    void copy_new_array_(T* new_data);
-    void allocate_new_array(std::size_t capacity);
+  void copy_new_array_(T *new_data);
+  void allocate_new_array(std::size_t capacity);
 };
 
 #include "Vector.cpp"
